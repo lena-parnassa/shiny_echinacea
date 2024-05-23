63969eb620954f85e8d66fd0a99f1187ed42d9aa
lena-parnassa <lenaparnassa2025@u.northwestern.edu>
2024-05-23 13:33:15 -0500

    the app isnt working at all but like in a way that feels promising

--- app.R
+++ app.R
@@ -4,37 +4,37 @@ library(shiny)
 library(tidyverse)
 library(DT)
 library(here)
+library(DataEditR)
+library(shinyWidgets)
 
 dailyData <- read_csv("data/datedDemo.csv")
-
-errorData <- read_csv("data/errorData.csv") 
-
-errorData <- rbind(errorData , c(1 , "none" , "lol this is fake" , NA)) 
-errorData <- rbind(errorData , c(1 , "none" , "lol this is also fake" , NA)) 
+errorData <- read_csv("data/errorData.csv")
+errorData <- rbind(errorData, c(1, "none", "lol this is fake", NA))
+errorData <- rbind(errorData, c(1, "none", "lol this is also fake", NA))
 
 errorVals <- as.numeric(errorData$demo.id)
-
-dailyData$error <- ifelse(dailyData$demo.id %in% errorData$demo.id , TRUE , FALSE)
+dailyData$error <- ifelse(dailyData$demo.id %in% errorData$demo.id, TRUE, FALSE)
 
 ui <- fluidPage(
   titlePanel("Daily Data Check"),
   sidebarLayout(
-    sidebarPanel("Filter Observations By:" , width = 3 ,
-                 fluidRow(
-                   column(10, selectInput("user_select", label = "Username", 
-                                          choices = c("None Selected", unique(dailyData$UserName)),
-                                          selected = "None Selected")), 
-                   column(10, selectInput("site_select", label = "Site Name",
-                                          choices = c("None Selected", unique(dailyData$siteName)),
-                                          selected = "None Selected")),
-                   column(10, selectInput("error_select", label = "Error Present",
-                                          choices = c("None Selected", unique(dailyData$error)),
-                                          selected = "None Selected"))
-                 )
+    sidebarPanel(
+      "Filter Observations By:", width = 3,
+      fluidRow(
+        column(10, selectInput("user_select", label = "Username", multiple = TRUE,
+                               choices = c("All", unique(dailyData$UserName)),
+                               selected = "All")),
+        column(10, selectInput("site_select", label = "Site Name", multiple = TRUE,
+                               choices = c("All", unique(dailyData$siteName)),
+                               selected = "All")),
+        column(10, selectInput("error_select", label = "Error Present", multiple = FALSE,
+                               choices = c("None Selected", unique(dailyData$error)),
+                               selected = "None Selected"))
+      )
     ),
     mainPanel(
       fluidRow(
-        column(8, dataTableOutput("table")),
+        column(8, dataEditOutput("data_editor")),
         column(4, uiOutput("infoBox"))
       )
     )
@@ -42,34 +42,29 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  rv <- reactiveValues(data = dailyData)
   user_selected <- reactive(input$user_select)
   site_selected <- reactive(input$site_select)
   error_selected <- reactive(input$error_select)
   
-  output$table <- renderDataTable({
-    filtered_data <- dailyData |>
+  observe({
+    filtered_data <- rv$data %>%
       filter(
-        (user_selected() == "None Selected" | UserName %in% user_selected()),
-        (site_selected() == "None Selected" | siteName %in% site_selected()),
+        (user_selected() == "All" | UserName %in% user_selected()),
+        (site_selected() == "All" | siteName %in% site_selected()),
         (error_selected() == "None Selected" | error %in% error_selected())
       )
-    datatable(filtered_data, selection = "multiple", options = list(
-      rowCallback = JS(
-        'function(row, data) {
-          if (data[["error"]] == "TRUE") {
-            $(row).addClass("row-error");
-          }
-        }'
-      )
-    )) |>
-      formatStyle("error", target = "row", backgroundColor = styleEqual(c(TRUE, FALSE), c("red", "white")))
+    
+    output$data_editor <- renderDataEdit({
+      data_edit(filtered_data, options = list(editable = TRUE))
+    })
   })
   
-  observeEvent(input$table_rows_selected, {
-    selected_row <- input$table_rows_selected
+  observeEvent(input$data_editor_rows_selected, {
+    selected_row <- input$data_editor_rows_selected
     if (length(selected_row) > 0) {
-      selected_demo_id <- dailyData$demo.id[selected_row]
-      error_info <- errorData |> filter(demo.id == selected_demo_id)
+      selected_demo_id <- rv$data$demo.id[selected_row]
+      error_info <- errorData %>% filter(demo.id == selected_demo_id)
       if (nrow(error_info) > 0) {
         output$infoBox <- renderUI({
           wellPanel(
